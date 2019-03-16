f95ff39c5b58821ca4747aa07fa664d29e166bd7
Masha <m20019999@gmail.com>
2019-03-16 16:42:14 +0300

    desktop + ipad

--- script.js
+++ script.js
@@ -30,14 +30,31 @@ $( ".lack p" ).click(function() {
   $( ".content" ).css( {"display": "block",
                         "opacity": "0"})
   $( ".content" ).css( {"transition": "5s"})
-
+  $( ".text-3" ).css( {"transition": "5s"})
+  $( ".text-2" ).css( {"transition": "5s"})
+  $( ".text-4" ).css( {"transition": "5s"})
 
   generator(100, 120, -1)
+
   setTimeout(function(){
       $( ".lack, .spin, .word, .next" ).css( {"display": "none" } )
+  }, 7000);
+  setTimeout(function(){
       $( ".content" ).css( {"opacity": "1"})
-  }, 6000);
+      $( ".text-1" ).css( {"opacity": "1"})
+
+  }, 1000);
+  setTimeout(function(){
+      $( ".text-2" ).css( {"opacity": "1"})
+
+  }, 5700);
+  setTimeout(function(){
+      $( ".text-3" ).css( {"opacity": "1"})
+      $( ".text-4" ).css( {"opacity": "1"})
+
+  }, 7000);
  })
+
 
  generator = function (start_x, start_y, direction = 1, step = 2, y_step = 150){
    cards_collector = $( ".cards-collector:first" )
@@ -48,18 +65,18 @@ $( ".lack p" ).click(function() {
    let c = 1100
    let x = start_x * 16.80
    console.log(x)
-   let x_step = step*1.68
-   let offset = Math.sqrt(Math.abs((start_y*9.71*b-c)/a))+x - width/4
+   let x_step = step * 1.68
+   let offset = Math.sqrt(Math.abs((start_y * 9.71 * b - c)/a)) + x - width/4
    let delay = 0
 
 
 
-   while ((x < 1680*1.5) && (x > -1680*0.2)) {
-     x = x + x_step*direction
+   while ((x < 1680 * 1.5) && (x > -1680 * 0.2)) {
+     x = x + x_step * direction
      let y = a * (x - offset ) * (x-offset) + c
      y = y/b
-     let y_real = y*height/971
-     let x_real = x*width/1680
+     let y_real = y * height/971
+     let x_real = x * width/1680
      let new_card = $("<div class='card'></div>")
      new_card.css({"top": y_real + "px",
                   "left": x_real + "px",
@@ -70,7 +87,6 @@ $( ".lack p" ).click(function() {
      if (y < 1150) {
        delay = delay + 20}
      else {
-       //c = c + y_step
        offset = x + direction * Math.sqrt(Math.abs((y * b -c)/a))
      }
    }}
